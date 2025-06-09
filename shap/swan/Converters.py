946a57b76fb83e0ab0444571f5a53a2455b21dda
rohith <srk240998@gmail.com>
2025-06-09 22:01:27 +0200

    Update evaluation results

--- shap/swan/Converters.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def inst_to_np(inst):
-    """ Convert instances to a numpy array """
-    inst_list = []
-    for ins in inst:
-        inst_list.append(ins.values)
-    return np.array(inst_list)
